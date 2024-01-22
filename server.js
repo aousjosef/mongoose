3ede321245887faa083bfdf3a3a14a579cf72267
aousjosef <aoussabah@gmail.com>
2024-01-22 03:43:01 +0100

    complted CRD routes

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+mongoose.connect("mongodb://localhost/myCV");
 
-app.listen(3000, () => { console.log(`server started at http://localhost:3000`);})
+const db = mongoose.connection;
+
+db.on("error", (e) => console.error(e));
+db.once("open", () => console.log("Ansulten via mongoose"));
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+
+app.use(express.json());
+
+const coursesRouter = require('./routes/courses');
+app.use('/courses', coursesRouter);
+
+app.listen(3000, () => {
+  console.log(`server started at http://localhost:3000`);
+});
